b34886c572591b2a52b75bc303b14bd594b155f3
eashamani <eeasha008@gmail.com>
2024-12-17 12:46:13 +0530

    Add files via upload

---
+++ vpn_configs/file.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "enter  yuor name :"
+read name
+echo "enter your age :"
+read age
+echo "enter your number :"
+read num
+echo "enter your department :"
+read department
+echo "enter your filename :"
+read filename
+echo "your name is $name"
+echo "your age is $age"
+echo "your number is $num"
+echo "your are from $department"
+echo "your file name is $file"
